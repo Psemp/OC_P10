1a0895c817c928e4cdcabba43c13dbb360786f2f
Pierre Sempere <pierre.sempere.01@gmail.com>
2021-07-10 03:20:14 +0200

    Travis files

---
+++ purbeurre/purbeurre/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'detault': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
