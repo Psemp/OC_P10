c2c312e00e15eea1fb27a3c3a3d19893f51e3377
Pierre Sempere <pierre.sempere.01@gmail.com>
2021-07-10 04:15:30 +0200

    forgot a comma, its 0415 gimme a break

--- purbeurre/purbeurre/settings/travis.py
+++ purbeurre/purbeurre/settings/travis.py
@@ -1,12 +1,12 @@
 from . import *
 
 DATABASES = {
-    'detault': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
-    }
+    },
 }
